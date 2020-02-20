39106cad142e2e73c3de2eae8cad994308a05cc2
Drewroen <drewroen@gmail.com>
2020-02-19 21:18:01 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+  });
+
+//app.listen(3000, () => console.log(`Listening on: 3000`));
+module.exports.handler = serverless(app);
