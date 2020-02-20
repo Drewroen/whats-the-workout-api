9aef32ceea8d01d42baae20fd80944023c7aa2bd
Drewroen <drewroen@gmail.com>
2020-02-20 00:11:01 -0600

    Working on CICD

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage prod --package \   $CODEBUILD_SRC_DIR/target/prod -v -r us-east-2
+serverless deploy --stage prod --package $CODEBUILD_SRC_DIR/target/prod -v -r us-east-2
