3a90ba73142ba93b214c773f8074f02c6c5edef3
Drewroen <drewroen@gmail.com>
2020-02-20 00:37:45 -0600

    Cleaning up!

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', function (req, res) {
+  res.send('The API is working')
+});
+
 app.get('/hello-world', function (req, res) {
   res.send('Hello World!')
 });
