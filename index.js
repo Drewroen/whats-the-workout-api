83d5a8db8005333e9b76b97d419fadc90ceb7b1e
Drewroen <drewroen@gmail.com>
2020-02-24 20:40:22 -0600

    First test of adding mongoose and mongodb

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
+const mongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -30,15 +31,78 @@ app.get('/xping', function (req, res) {
   var pingResponse = {
     message: 'The service is healthy'
   }
-  res.send(pingResponse)
+  res.send(pingResponse);
 });
 
 app.get('/xping-auth', checkJwt, function (req, res) {
   var pingResponse = {
     message: 'The service is healthy. You are logged in'
   }
-  res.send(pingResponse)
+  res.send(pingResponse);
 });
 
+async function decrypt(source) {
+  const params = {
+    CiphertextBlob: Buffer.from(source, 'base64'),
+  };
+  const { Plaintext } = await kms.decrypt(params).promise();
+  return Plaintext.toString();
+}
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2' });
+const encrypted = process.env['MONGO_URL'];
+const kms = new AWS.KMS();
+decrypt(encrypted).then(decrypted => {console.log(decrypted); mongoose.connect(decrypted, {
+  useNewUrlParser: true
+})});
+
+var Workout = new mongoose.Schema({
+  workoutId: {
+    type: String,
+    required: false
+  },
+  userId: {
+    type: String,
+    required: false
+  },
+  name: {
+    type: String,
+    required: false
+  },
+  details: {
+    type: String,
+    required: false
+  }
+});
+
+var WorkoutModel = mongoose.model('WorkoutModel', Workout, 'workouts');
+module.exports = WorkoutModel;
+
+app.get('/workouts', checkJwt, async function (req, res) {
+  const workouts = await WorkoutModel.find({});
+  try {
+    res.send(workouts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post('/workouts', checkJwt, async function (req, res) {
+  const workout = new WorkoutModel({
+    workoutId: "bla",
+    userId: "bla",
+    name: "Workout",
+    details: "BLA"
+  });
+  try {
+    await workout.save();
+    res.send(workout);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
 //app.listen(3000, () => console.log(`Listening on: 3000`));
+
 module.exports.handler = serverless(app);
